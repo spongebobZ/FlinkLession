447ffb41e38891ef1d607bc31cc8dcae69896f21
wangruiqi <jolinyl.zhu@hkairlines.com>
2024-10-03 11:03:24 +0800

    delete some dir

--- src/main/java/conf/MysqlConf.java
+++ src/main/java/conf/MysqlConf.java
@@ -6,6 +6,6 @@ public class MysqlConf {
     public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     public static final String URL = "jdbc:mysql://localhost:3306/flink";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "Ab12345678#";
+    public static final String PASSWORD = "";
     public static final int FETCH_SIZE = 5000;
 }
